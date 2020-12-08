ca3681c6c67f5a30b6bc3a4f0049415430b6e68c
talesCPV <tales@flexibus.com.br>
2020-12-08 16:49:39 -0300

    by_script

---
+++ clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Clone files to Github - "https://github.com/talesCPV/iron_age.git"
+
+cd ..
+
+rm -rf iron_age/
+
+git init
+
+git clone "https://github.com/talesCPV/iron_age.git"
+
+cd iron_age/
--- sketchs/ironage.js
+++ sketchs/ironage.js
@@ -1,6 +1,7 @@
-// SPACE INVADERS
+// IRON AGE 2020
 let screen = [800,600];
 let sprites;
+let back_sprites;
 let pixel = 3; // tamanho do pixel
 let fps = [0,45]; // 1- contador | 2- qtd de frames
 let score = 0;
@@ -8,6 +9,7 @@ let player_speed = 3;
 let sprite_name = "main"
 let shooting = [];
 let bombing = [];
+let scene = [];
 
 
 
@@ -21,15 +23,15 @@ let p1 = player;
 let enemy = [];
 
 function preload() {
-
     sprites = loadJSON('assets/ironage.json');
-
+    back_sprites = loadJSON('assets/background.json');
 }
 
 function setup() {
     createCanvas(screen[0], screen[1]);
     textSize(20);
     textAlign(10, 10);
+    scene.push(new Background("top","cavern","ceiling_01",48));
 
 }
 
@@ -50,37 +52,61 @@ function draw() {
     }
 
 
+    draw_background();
 
     joystick();
-    draw_sprite(p1.x,p1.y,"player");  
-    
+    draw_sprite(p1.x,p1.y,"player",sprite_name);
+
+}
+
+function draw_pixel(x,y,N){
+    let x2 = pixel;
+    let y2 = pixel;
+
+    for(let i=0;i<N.length;i++){
+        let color = N[i].color;
+        fill(color);
+        for(let j=0; j<N[i].data.length; j++){
+            let x1 = x + N[i].data[j][0]*pixel;
+            let y1 = y - N[i].data[j][1]*pixel;
+            if(N[i].data[j].length > 2){
+              x2 =  N[i].data[j][2]*pixel;
+              y2 =  N[i].data[j][3]*pixel;
+            }
+            rect(x1,y1, x2, y2);
+        }
+    }
 }
 
 
+function draw_sprite(x,y,EN,SN){
+    let dots = sprites[EN][SN].dots
+    draw_pixel(x,y,dots);
 
-function draw_sprite(x,y,EN){
-    dots = sprites[EN][sprite_name].dots
-    for(let i=0;i<dots.length;i++){ 
-        let color = dots[i].color;
-        fill(color);
-        for(let j=0; j<dots[i].data.length; j++){
-            let x1 = x + dots[i].data[j][0]*pixel;
-            let y1 = y - dots[i].data[j][1]*pixel;
-            rect(x1,y1, pixel, pixel);
-        }
-    }
-    lines = sprites[EN][sprite_name].lines
-    for(let i=0;i<lines.length;i++){ 
-        let color = lines[i].color;
-        fill(color);
-        for(let j=0; j<lines[i].data.length; j++){
-            let x1 = x + lines[i].data[j][0]*pixel;
-            let y1 = y - lines[i].data[j][1]*pixel;
-            let x2 =  lines[i].data[j][2]*pixel;
-            let y2 =  lines[i].data[j][3]*pixel;
-            rect(x1,y1, x2, y2);
-        }
-    }    
+    let lines = sprites[EN][SN].lines
+    draw_pixel(x,y,lines);
+}
+
+function draw_background(){
+
+  for(let i=0;i<scene.length;i++){
+
+    let x = scene[i].x;
+    let y = scene[i].y;
+    let kind = scene[i].kind;
+    let name = scene[i].name;
+
+    let dots = back_sprites["cavern"]["ceiling_01"].dots;
+    let lines = back_sprites[kind][name].lines;
+
+    draw_pixel(x,y,dots);
+    draw_pixel(x,y,lines);
+
+    scene[i].move();
+
+
+  }
+
 }
 
 
@@ -91,8 +117,7 @@ function Shoting(x,y){
 
 Shoting.prototype.draw = function(){
     this.x += 6;
-    sprite_name = "main";
-    draw_sprite(this.x,this.y,"weapons")
+    draw_sprite(this.x,this.y,"weapons","main")
 
     if(this.x > width + 10){
         shooting.splice(0,1);
@@ -106,15 +131,16 @@ function Bombing(x,y){
 }
 
 Bombing.prototype.draw = function(){
+    let SPT;
     if(this.x < this.start[0] + 8 * pixel){
         this.x += 1;
-        sprite_name = "bomb";
+        SPT = "bomb";
     }else{
         this.x += 1;
         this.y += 3;
-        sprite_name = "falling_bomb";
+        SPT = "falling_bomb";
     }
-    draw_sprite(this.x,this.y,"weapons")
+    draw_sprite(this.x,this.y,"weapons",SPT)
 
     if(this.y > height + 10){
         shooting.splice(0,1);
@@ -122,22 +148,48 @@ Bombing.prototype.draw = function(){
 
 }
 
+function Background(dir,kind,name,width){
+  this.dir = dir;
+  this.name = name;
+  this.kind = kind;
+  this.width = width;
+  this.count = 0;
+  this.x =  screen[0]+100 ;
+  if(dir == "top"){
+    this.y = 70;
+  }else{
+    this.y = height;
+  }
+}
+
+Background.prototype.move = function(){
+  this.x --;
+  this.count ++;
+
+  if(this.count == (this.width * pixel) - 3 * pixel ) {
+    scene.push(new Background(this.dir, this.kind,this.name, this.width));
+  }
+  if(this.x <= -50*pixel){
+    scene.splice(0,1);
+  }
+
+}
 
 function joystick(){
-    if(keyIsDown(LEFT_ARROW) && p1.x > 20) {    
+    if(keyIsDown(LEFT_ARROW) && p1.x > 20) {
         p1.x -= player_speed;
-    }else if(keyIsDown(RIGHT_ARROW) && p1.x < width - 100) {    
+    }else if(keyIsDown(RIGHT_ARROW) && p1.x < width - 100) {
         p1.x += player_speed;
     }
 
-    if(keyIsDown(UP_ARROW) && p1.y > 50) {    
+    if(keyIsDown(UP_ARROW) && p1.y > 50) {
         p1.y -= player_speed;
         sprite_name = "up";
-    }else if(keyIsDown(DOWN_ARROW) && p1.y < height - 40) {    
+    }else if(keyIsDown(DOWN_ARROW) && p1.y < height - 40) {
         p1.y += player_speed;
         sprite_name = "down";
     }else{
-        sprite_name = "main";        
+        sprite_name = "main";
     }
 }
 
@@ -153,6 +205,5 @@ function keyPressed() {
     }
     if( keyIndex == 120 ){ // SPACE OR UP => TURN THE PIECE
         shooting.push(new Bombing(player.x,player.y));
-    }    
+    }
 }
-

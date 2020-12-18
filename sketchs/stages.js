dff2e0eb29f679030cab2bee17c98b40ece1fae4
talesCPV <tales@flexibus.com.br>
2020-12-18 15:39:38 -0300

    by_script_tales

--- sketchs/stages.js
+++ sketchs/stages.js
@@ -72,7 +72,8 @@ function stage_select(sel_stage){
         text("FEAR OF THE DARK", 550, 550, 200, 150);
 
 
-        draw_sprite(202,120,pl_sprites.splash["eddie_egito"]);
+        draw_sprite(202,120,pl_sprites.splash["eddie_trooper"]);
+        draw_sprite(202,298,pl_sprites.splash["eddie_egito"]);
 
 
 
